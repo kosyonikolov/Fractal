6c0185747770192efc35801b66d78498fd1e49c8
Kosyo Nikolov <kosyo.nikolov@protonmail.com>
2020-06-19 23:21:39 +0300

    improve scripts

---
+++ test/spam_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/kosyo/Documents/Fractal/test
+
+date >> spam.log
+
+./time.sh server 2048x2048 -1:1:-1:1 256x16 8 12 16 20 24 28 32
--- test/time.sh
+++ test/time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USAGE_MSG="Usage: ./time.sh [pc|dev] [size] [complex size] [granularity] <thread counts...>"
+USAGE_MSG="Usage: ./time.sh [pc|dev|server] [size] [complex size] [chunk size] <thread counts...>"
 
 if [[ $# -lt 5 ]]
 then
@@ -16,25 +16,31 @@ if [[ $TYPE == "dev" ]]
 then
     #./device_upload.sh
     CMD=./run_device.sh
+elif [[ $TYPE == "server" ]]
+then
+    CMD=./run_server.sh
 fi
 
 SIZE="$2"
 C_SIZE="$3"
-GRANULARITY="$4"
-ATTEMPTS=5
+CHUNK_SIZE="$4"
+ATTEMPTS=7
+
+NOW=$(date +%H%M%S)
+RUN_ID="${TYPE}_${CHUNK_SIZE}_${NOW}"
 
 # write header
-OUT_FILE="stat_${TYPE}_${GRANULARITY}.csv"
-echo "threads,granularity,time" > "$OUT_FILE"
+OUT_FILE="stat_${RUN_ID}.csv"
+echo "threads,chunksize,time" > "$OUT_FILE"
 
-LOG_FILE="${TYPE}_${GRANULARITY}.log"
+LOG_FILE="${RUN_ID}.log"
 date > "$LOG_FILE"
 
 shift 4
 
 for T_COUNT in "$@"
 do
-    G=$GRANULARITY
+    G=$CHUNK_SIZE
     if [[ $T_COUNT -eq 1 ]]
     then
         G=1
@@ -45,8 +51,8 @@ do
     while [[ $ITER -lt $ATTEMPTS ]]
     do
         echo "Attempt $ITER / $ATTEMPTS"
-        echo $CMD -s $SIZE -r $C_SIZE -t $T_COUNT -g $G -v 1
-        echo $CMD -s $SIZE -r $C_SIZE -t $T_COUNT -g $G -v 1 >> "$LOG_FILE"
+        echo $CMD -s $SIZE -r $C_SIZE -t $T_COUNT -c $G -v 1
+        echo $CMD -s $SIZE -r $C_SIZE -t $T_COUNT -c $G -v 1 >> "$LOG_FILE"
         OUTPUT=$($CMD -s $SIZE -r $C_SIZE -t $T_COUNT -g $G -v 1)
         echo "$OUTPUT" >> "$LOG_FILE"
 
